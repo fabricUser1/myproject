8488d03be7ec39f55441c80c6f5c624eee83dcce
fabricUser1 <fb1@ytta.in>
2025-02-21 13:30:22 +0530

    Committing 2 items from workspace 077cd378-40a5-44c8-9714-207c8be826f0

---
+++ dev/Transform data.Notebook/notebook-content.py
@@ -0,0 +1,39 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "316a267f-948d-4685-9915-afcf2bd59851",
+# META       "default_lakehouse_name": "workinglh",
+# META       "default_lakehouse_workspace_id": "077cd378-40a5-44c8-9714-207c8be826f0"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+df = spark.read.format("csv").option("header", "true").load("Files/bronze/sales.csv")
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df.write.format("delta").saveAsTable("salesorders")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
